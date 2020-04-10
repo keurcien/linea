1ba28db65f1e6f9d8e6661e5721946bdfbcb2187
keurcien <keurcien.luu@gmail.com>
2020-04-10 19:00:58 +0200

    Fixed admin url

--- linea/urls.py
+++ linea/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('scale.urls')),
+    path('/', include('scale.urls')),
     path('admin/', admin.site.urls),
     path('scale/', include('scale.urls'))
 ]
