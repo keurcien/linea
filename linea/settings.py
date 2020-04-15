5316651cd2020501b671ced85a0d07c52f9466ba
keurcien <keurcien.luu@gmail.com>
2020-04-15 10:36:36 +0200

    Changed wording

--- linea/settings.py
+++ linea/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
